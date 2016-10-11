220a01344a50d4ab5394e4262b957e3821cf3cfb
mauro.larese <mauro.larese@mm-one.com>
2016-10-11 15:58:09 +0200

    update gpdati

--- src/com/mmone/gpdati/config/GpDatiXmlRpcConfigurator.java
+++ src/com/mmone/gpdati/config/GpDatiXmlRpcConfigurator.java
@@ -33,4 +33,15 @@ public class GpDatiXmlRpcConfigurator implements XmlRpcConfigurator {
             throw new XmlRpcConfigErrorException("Error configuring client "+ex.getMessage());
         }
     }
+    
+    public static void main(String[] args) {
+        try {
+            XmlRpcClient client = new XmlRpcClient("");
+            client.setBasicAuthentication("" , "");
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(GpDatiXmlRpcConfigurator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
 }
